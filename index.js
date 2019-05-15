f6f3f1691133e48499d7401f1492d11d0981db3d
bui quang huy <Codatduoc@gmail.com>
2019-05-16 03:44:19 +0700

    Thêm back-end chu de

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ app.use(express.static(__dirname + '/views'));
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-   host: "db4free.net",
+    host: "db4free.net",
     user: "thanhan1181999",
     password: "77621176211",
     database: "news_data",
     port: "3306"
 
-   /* host: "localhost",
-    user: "root",
-    password: "",
-    database: "news_data",
-    port: "3307"*/
+    /* host: "localhost",
+     user: "root",
+     password: "",
+     database: "news_data",
+     port: "3307"*/
 });
 
 con.connect();
@@ -28,66 +28,67 @@ con.connect();
 app.get("/", function(req, res) {
     var sql = "select * from bongda where hot='YY' " +
         "union select * from kinhdoanh where hot='YY' " +
-         "union select * from hitech where hot='YY' " +
+        "union select * from hitech where hot='YY' " +
         "union select * from thitruong where hot='YY' " +
         "union select * from showbiz where hot='YY' " +
         "union select * from suckhoe where hot='YY' " +
         "union select * from phaidep where hot='YY'  " +
         "union select * from amthuc where hot='YY' " +
-         "union select * from thegioi where hot='YY' "  ;
+        "union select * from thegioi where hot='YY' ";
     con.query(sql, function(error, results, fields) {
         if (error) throw error;
         else {
-            res.render('home2', { results });//results laf 
+            res.render('home2', { results }); //results la
         }
     });
 });
 app.post('/', urlencodedParser, function(req, res) {
-        var word = req.body.word;
-        word = word.toLowerCase();
-        var sql = "select * from bongda " +
-            "union select * from kinhdoanh " +
-            "union select * from thitruong " +
-            "union select * from suckhoe " +
-            "union select * from hitech " +
-            "union select * from showbiz " +
-            "union select * from thegioi " +
-            "union select * from thethao " +
-            "union select * from phaidep " +
-            "union select * from amthuc ";
-        var pos = [];
-        con.query(sql, function(error, results, fields) {
-            if (error) throw error;
-            else {
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].name.toLowerCase().search(word) != -1) {
-                        pos.push(results[i]);
-                    }
+    var word = req.body.word;
+    word = word.toLowerCase();
+    var sql = "select name,time,author,image1,image2,type1,type,id from bongda " +
+        "union select name,time,author,image1,image2,type1,type,id from kinhdoanh " +
+        "union select name,time,author,image1,image2,type1,type,id from thitruong " +
+        "union select name,time,author,image1,image2,type1,type,id from suckhoe " +
+        "union select name,time,author,image1,image2,type1,type,id from hitech " +
+        "union select name,time,author,image1,image2,type1,type,id from showbiz " +
+        "union select name,time,author,image1,image2,type1,type,id from thegioi " +
+        "union select name,time,author,image1,image2,type1,type,id from thethao " +
+        "union select name,time,author,image1,image2,type1,type,id from phaidep " +
+        "union select name,time,author,image1,image2,type1,type,id from amthuc ";
+    var pos = [];
+    con.query(sql, function(error, results, fields) {
+        if (error) throw error;
+        else {
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].name.toLowerCase().search(word) != -1) {
+                    pos.push(results[i]);
                 }
-                res.render('ketquatimkiem', { pos });
             }
-        })
+            res.render('ketquatimkiem', { pos });
+        }
     })
+})
 
-    
-    //góp ý-------------------------------------------------------
+
+//góp ý-------------------------------------------------------
 app.get('/feedback', function(req, res) {
     res.render('feedback');
 });
 app.post('/feedback', urlencodedParser, function(req, res) {
     var name = req.body.name;
     var address = req.body.address;
+    var phone = req.body.phone;
     var feedback = req.body.feedback;
     //câu lệnh chèn
-    var sql = "insert into gopy(name,address,feedback) values(N'" + name + "',N'" + address + "',N'" + feedback + "')";
+    var sql = "insert into gopy(name,address,phone,feedback) values(N'" + name + "',N'" + address + "',N'" + phone + "',N'" + feedback + "')";
     con.query(sql, function(error, results, fields) {
         if (error) throw error;
         else {
-            res.render('return_result');
+            console.log(sql);
+            res.render('feedback_result');
         }
     });
 });
-
 
 // Giao Diện giới thiệu
 app.get('/gioithieu', function(req, res) {
@@ -106,34 +107,31 @@ app.get('/:type', function(req, res) {
     var sql = "auto";
     switch (x) {
         case "bongda":
-            sql = "select * from bongda limit 5";
+            sql = " select * from bongda union select * from kinhdoanh  union select * from thitruong  ";
             break;
         case "kinhdoanh":
-            sql = "select * from kinhdoanh limit 5";
+            sql = "select * from kinhdoanh union select * from suckhoe  union select * from thitruong ";
             break;
         case "thitruong":
-            sql = "select * from thitruong limit 5";
+            sql = "select * from thitruong union select * from suckhoe  union select * from hitech ";
             break;
         case "suckhoe":
-            sql = "select * from suckhoe limit 5";
+            sql = "select * from suckhoe union select * from hitech  union select * from showbiz ";
             break;
         case "hitech":
-            sql = "select * from hitech limit 5";
+            sql = "select * from hitech union select * from showbiz  union select * from thegioi ";
             break;
         case "showbiz":
-            sql = "select * from showbiz limit 5";
+            sql = "select * from showbiz union select * from thegioi  union select * from phaidep";
             break;
         case "thegioi":
-            sql = "select * from thegioi limit 5";
-            break;
-        case "thethao":
-            sql = "select * from thethao limit 5";
+            sql = "select * from thegioi union select * from phaidep  union select * from amthuc ";
             break;
         case "phaidep":
-            sql = "select * from phaidep limit 5";
+            sql = "select * from phaidep union select * from amthuc  union select * from bongda ";
             break;
         case "amthuc":
-            sql = "select * from amthuc limit 5";
+            sql = "select * from amthuc union select * from bongda  union select * from kinhdoanh";
             break;
     }
     if (sql == "auto") res.redirect('/')
@@ -150,20 +148,18 @@ app.get('/:type', function(req, res) {
 app.get('/:type/:id', function(req, res) {
     var x = req.params.id;
     var y = req.params.type;
-    if(y=="bongda" ||y=="kinhdoanh"||y=="thitruong"||y=="suckhoe"||y=="hitech"|| 
-    y=="showbiz"||y=="thegioi"||y=="thethao"||y=="phaidep"||y=="amthuc" )
-    {
+    if (y == "bongda" || y == "kinhdoanh" || y == "thitruong" || y == "suckhoe" || y == "hitech" ||
+        y == "showbiz" || y == "thegioi" || y == "thethao" || y == "phaidep" || y == "amthuc") {
         var sql = "select * from " + y + " where id=" + x +
-        " union select * from " + y + " where hot='Y'";
+            " union select * from " + y + " where hot='YY' or hot='Y' ";
 
-    con.query(sql, function(error, results, fields) {
-        if (error) throw error;
-        else {
-            res.render('GiaoDienBaiViet', { results });
-        }
-    });
-    }
-    else res.redirect('/')
+        con.query(sql, function(error, results, fields) {
+            if (error) throw error;
+            else {
+                res.render('GiaoDienBaiViet', { results });
+            }
+        });
+    } else res.redirect('/')
 });
 
 app.listen(process.env.PORT || 3000);
